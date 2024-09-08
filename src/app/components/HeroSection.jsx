40d438751ba802a2762a1aa9055859b562a982e5
t-anar514 <132026601+t-anar514@users.noreply.github.com>
2024-09-08 19:33:56 +0800

    Update HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -22,7 +22,7 @@ const HeroSection = () => {
             <br></br>
             <TypeAnimation
               sequence={[
-                "Hishigdalai",
+                "Anar",
                 1000,
                 "Web Developer",
                 1000,
