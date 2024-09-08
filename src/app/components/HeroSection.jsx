20e12e7cdc880f0defe2e5c6b8d6cd9f4eadbeab
tamiranar514 <tamiranar514@gmail.com>
2024-09-08 19:28:46 +0800

    anar

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -22,7 +22,7 @@ const HeroSection = () => {
             <br></br>
             <TypeAnimation
               sequence={[
-                "Hishigdalai",
+                "Anar",
                 1000,
                 "Web Developer",
                 1000,
@@ -63,11 +63,11 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-[#181818] dark:bg-slate-200 w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+          <div className="  w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/hero-image.png"
+              src="/images/image.png"
               alt="hero image"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              className="absolute rounded-full transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
               height={300}
             />
@@ -79,3 +79,5 @@ const HeroSection = () => {
 };
 
 export default HeroSection;
+
+//bg-[#181818] dark:bg-slate-200
