2b033b9acf29577697b86e60b0524a10fe37fb65
tamiranar514 <tamiranar514@gmail.com>
2026-01-28 22:27:32 +0900

    hhe

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -46,7 +46,7 @@ const AboutSection = () => {
   return (
     <section className="text-white dark:text-black" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/Anar.png" width={500} height={500} />
+        <Image src="/images/Anar.png" width={500} height={500} alt="About Anar" />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white dark:text-black mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
